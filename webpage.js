bf6cd2cf670341fff85a3e32402c0a0631ecb3b3
lilianastrajescu <liliana.strajescu96@gmail.com>
2022-10-19 11:35:32 +0200

    PHP submit form

--- webpage.js
+++ webpage.js
@@ -1,5 +1,10 @@
-const form = document.querySelector("myForm")
+/*const form = document.querySelector("myForm")
 myForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    
-})
+    let request = new XMLDocument();
+    request.open("post", "login.php");
+    request.onload = function(){
+        console.log(request.responseText);
+    }
+    request.isSameNode(new FormData(myForm));
+})*/
