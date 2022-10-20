4418855e08783da474e695badc309ffc9cd95f1a
lilianastrajescu <liliana.strajescu96@gmail.com>
2022-10-20 11:41:18 +0200

    Button go top on the page

---
+++ website-app.js
@@ -0,0 +1,21 @@
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When scroll down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When click on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
